e31c5062e341cf39a0a8dcc27466628e8720b172
starkovaleksey2020 <starkovaleksey2017@gmail.com>
2021-07-07 17:20:19 +0800

    added: genres slug page parameters title

--- src/main/java/com/example/MyBookShopApp/controllers/GenresPageController.java
+++ src/main/java/com/example/MyBookShopApp/controllers/GenresPageController.java
@@ -1,8 +1,10 @@
 package com.example.MyBookShopApp.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/")
@@ -14,6 +16,10 @@ public class GenresPageController {
     }
 
     @GetMapping("/genres/slug")
-    public String slugPage() { return "slug_genres"; }
+    public String slugPage(@RequestParam(value = "genresName", required = false) String genresName,
+                           Model model) {
+        model.addAttribute("genresName", genresName);
+        return "slug_genres";
+    }
 
 }
